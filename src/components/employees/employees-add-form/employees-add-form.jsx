46e0b804f6515806ff44c7c3a5675242848c77c8
rodion <rodion.rv111@icloud.com>
2021-10-02 16:06:10 +0300

    managed component

--- src/components/employees/employees-add-form/employees-add-form.jsx
+++ src/components/employees/employees-add-form/employees-add-form.jsx
@@ -1,27 +1,54 @@
+import { Component } from 'react'
 import './employees-add-form.css'
 
-const EmployeesAddForm = () => {
-  return (
-    <div className='app-add-form'>
-      <h3>Добавьте нового сотрудника</h3>
-      <form className='add-form d-flex'>
-        <input
-          type='text'
-          className='form-control new-post-label'
-          placeholder='Как его зовут?'
-        />
-        <input
-          type='number'
-          className='form-control new-post-label'
-          placeholder='З/П в $?'
-        />
+class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      salary: '',
+    }
+  }
 
-        <button type='submit' className='btn btn-outline-light'>
-          Добавить
-        </button>
-      </form>
-    </div>
-  )
+  // один обработчик для двух инпутов
+  onValueChange = (event) => {
+    const targetValue = event.taget.value
+    const targetName = event.taget.name
+    this.setState({
+      [targetName]: targetValue,
+    })
+  }
+
+  render() {
+    const {name, salary} = this.state
+    return (
+      <div className='app-add-form'>
+        <h3>Добавьте нового сотрудника</h3>
+        <form className='add-form d-flex'>
+          <input
+            onChange={this.onValueChange}
+            name='name'
+            value={name}
+            type='text'
+            className='form-control new-post-label'
+            placeholder='Как его зовут?'
+          />
+          <input
+            onChange={this.onValueChange}
+            name='salary'
+            value={salary}
+            type='number'
+            className='form-control new-post-label'
+            placeholder='З/П в $?'
+          />
+
+          <button type='submit' className='btn btn-outline-light'>
+            Добавить
+          </button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default EmployeesAddForm
